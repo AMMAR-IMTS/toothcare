62d77cefe3632c067769999f8ff8e275563ab363
AMMAR <githhubimts@gmail.com>
2024-10-22 21:47:51 +0530

    inital

--- config.php
+++ config.php
@@ -13,8 +13,7 @@ date_default_timezone_set('Asia/Colombo');
 
 function current_domain()
 {
-    return protocol() . $_SERVER['HTTP_HOST'] . '/toothcare';
-    // return protocol() . $_SERVER['HTTP_HOST'];
+    return protocol() . $_SERVER['HTTP_HOST'] . '/tooth-care-imts';
 }
 
 function currentUrl()
